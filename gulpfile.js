8571f9e1ae572be35240f501b535ad5b51c5f772
Scott Smith <mail@ScottHSmith.com>
2018-08-14 13:38:21 -0700

    Update code style for ESLint.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const gulp       = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const postcss    = require('gulp-postcss');
-const concat     = require('gulp-concat');
+const gulp       = require( 'gulp' );
+const sourcemaps = require( 'gulp-sourcemaps' );
+const postcss    = require( 'gulp-postcss' );
+const concat     = require( 'gulp-concat' );
 // Use uglify-es minifier with gulp-uglify for ES2015 support.
-const composer   = require('gulp-uglify/composer');
-const uglifyes   = require('uglify-es');
-const minify     = composer(uglifyes, console);
-const sprite     = require('gulp-svg-sprite');
+const composer   = require( 'gulp-uglify/composer' );
+const uglifyes   = require( 'uglify-es' );
+const minify     = composer( uglifyes, console );
+const sprite     = require( 'gulp-svg-sprite' );
 
 const paths = {
 	styles: {
@@ -16,44 +16,44 @@ const paths = {
 		dest: 'assets/styles/build/'
 	},
 	scripts: {
-		src: ['node_modules/boomsvgloader/dist/js/boomsvgloader.js', 'assets/scripts/*.js', '!assets/scripts/build/**'],
+		src: [ 'node_modules/boomsvgloader/dist/js/boomsvgloader.js', 'assets/scripts/*.js', '!assets/scripts/build/**' ],
 		dest: 'assets/scripts/build/'
 	},
 	sprites: {
-		src: ['assets/images/icons/*.svg', '!assets/images/icons/icon-sprite.svg'],
+		src: [ 'assets/images/icons/*.svg', '!assets/images/icons/icon-sprite.svg' ],
 		dest: 'assets/images/icons'
 	}
 };
 
 const processors = [
-	require('postcss-import'),
-	require('postcss-nested'),
-	require('postcss-custom-properties'),
-	require('css-mqpacker')({sort: true}),
-	require('autoprefixer'),
-	require('postcss-normalize'),
-	require('cssnano')({autoprefixer: false, reduceIdents: false}) // Autoprefixer has just been run, don't do it again; reduceIdents is pretty unsafe.
+	require( 'postcss-import' ),
+	require( 'postcss-nested' ),
+	require( 'postcss-custom-properties' )( { warnings: true } ),
+	require( 'css-mqpacker' )( { sort: true } ),
+	require( 'autoprefixer' ),
+	require( 'postcss-normalize' ),
+	require( 'cssnano' )( { preset: 'default' } )
 ];
 
 function styles() {
-	return gulp.src(paths.styles.src)
-		.pipe(sourcemaps.init())
-			.pipe(postcss(processors))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(paths.styles.dest));
+	return gulp.src( paths.styles.src )
+		.pipe( sourcemaps.init() )
+			.pipe( postcss( processors ) )
+		.pipe( sourcemaps.write( './' ) )
+		.pipe( gulp.dest( paths.styles.dest ) );
 }
 
 function scripts() {
-	return gulp.src(paths.scripts.src)
-		.pipe(sourcemaps.init())
-			.pipe(concat('app.js'))
-			.pipe(minify())
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(paths.scripts.dest));
+	return gulp.src( paths.scripts.src )
+		.pipe( sourcemaps.init() )
+			.pipe( concat( 'app.js' ) )
+			.pipe( minify() )
+		.pipe( sourcemaps.write( './' ) )
+		.pipe( gulp.dest( paths.scripts.dest ) );
 }
 
 function sprites() {
-	var options = {
+	const options = {
 		mode: {
 			symbol: { // Create a «symbol» sprite.
 				dest: '.', // Don't create 'symbols/' directory.
@@ -63,25 +63,25 @@ function sprites() {
 		}
 	};
 
-	return gulp.src(paths.sprites.src)
-		.pipe(sprite(options))
-		.pipe(gulp.dest(paths.sprites.dest));
+	return gulp.src( paths.sprites.src )
+		.pipe( sprite( options ) )
+		.pipe( gulp.dest( paths.sprites.dest ) );
 }
 
 function watch() {
-	gulp.watch(paths.styles.src, styles);
-	gulp.watch(paths.scripts.src, scripts);
-	gulp.watch(paths.sprites.src, sprites);
+	gulp.watch( paths.styles.src, styles );
+	gulp.watch( paths.scripts.src, scripts );
+	gulp.watch( paths.sprites.src, sprites );
 }
 
-// Workflows
+/* Workflows */
 // $ gulp: Builds and watches for changes. The works.
-const defaultTask = gulp.parallel(styles, scripts, sprites, watch);
+const defaultTask = gulp.parallel( styles, scripts, sprites, watch );
 
 // $ gulp build: Builds for deployments.
-const buildTask = gulp.parallel(styles, scripts, sprites);
+const buildTask = gulp.parallel( styles, scripts, sprites );
 
-// Exports
+/* Exports */
 // Externalise individual tasks.
 exports.styles = styles;
 exports.scripts = scripts;
